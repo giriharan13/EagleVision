9eecec3c1af2be6fe0ccd77ba35c19040491435a
giriharan13 <lionking0304channel@gmail.com>
2024-02-25 15:04:25 +0530

    Backend update: ShopReview.java
    added a model ShopReview.java

---
+++ Backend/src/main/java/com/eaglevision/Backend/model/ShopReview.java
@@ -0,0 +1,74 @@
+package com.eaglevision.Backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@PrimaryKeyJoinColumn(name="reviewId")
+public class ShopReview extends Review {
+	
+	@Size(min=5,max=100)
+	private String comment;
+	
+	@Column(columnDefinition = "int default 0")
+	private Integer likes;
+	
+	@Column(columnDefinition = "int default 0")
+	private Integer dislikes;
+	
+	@ManyToOne
+	private Shop shop;
+	
+	public ShopReview() {
+		super();
+	}
+	
+	public ShopReview(Integer stars,Boolean isEdited,String comment,Integer likes,Integer dislikes) {
+		super(stars,isEdited);
+		this.comment = comment;
+		this.likes = likes;
+		this.dislikes = dislikes;
+	}
+	
+	public ShopReview(Integer stars,String comment,Integer likes,Integer dislikes) {
+		super(stars);
+		this.comment = comment;
+		this.likes = likes;
+		this.dislikes = dislikes;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Integer getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
+
+	public Integer getDislikes() {
+		return dislikes;
+	}
+
+	public void setDislikes(Integer dislikes) {
+		this.dislikes = dislikes;
+	}
+
+	public Shop getShop() {
+		return shop;
+	}
+
+	public void setShop(Shop shop) {
+		this.shop = shop;
+	}
+}
