009c69756b218aac91e1d1ef6db3bb2ec6daa91d
giriharan13 <lionking0304channel@gmail.com>
2024-03-15 22:29:11 +0530

    Backend: VendorService.java
    added a service VendorService

---
+++ Backend/src/main/java/com/eaglevision/Backend/service/VendorService.java
@@ -0,0 +1,46 @@
+package com.eaglevision.Backend.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eaglevision.Backend.model.Shop;
+import com.eaglevision.Backend.model.Vendor;
+import com.eaglevision.Backend.repository.VendorRepository;
+
+@Service
+public class VendorService {
+	
+	private VendorRepository vendorRepository;
+	
+	@Autowired
+	public VendorService(VendorRepository vendorRepository) {
+		this.vendorRepository = vendorRepository;
+	}
+	
+	public Vendor createVendor(Vendor vendor) {
+		return this.vendorRepository.save(vendor);
+	}
+	
+	public Vendor getVendorById(Integer id) {
+		return this.vendorRepository.findById(id).get();
+	}
+	
+	public List<Vendor> getVendors(){
+		return this.vendorRepository.findAll();
+	}
+	
+	public Vendor updateVendor(Vendor updatedVendor,Integer userId) {
+		Vendor vendor = vendorRepository.findById(userId).get();
+		vendor.setUserName(updatedVendor.getUserName());
+		vendor.setPhoneNumber(updatedVendor.getPhoneNumber());
+		vendor.setDateOfBirth(updatedVendor.getDateOfBirth());
+		return this.vendorRepository.save(vendor);
+	}
+	
+	public void deleteVendor(Integer id) {
+		this.vendorRepository.deleteById(id);
+	}
+	
+}
