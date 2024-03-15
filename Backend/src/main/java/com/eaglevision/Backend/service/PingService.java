f85821dbc1b6571d9c64c933b939221783a13b20
giriharan13 <lionking0304channel@gmail.com>
2024-03-15 22:27:08 +0530

    Backend: PingService.java
    added a service PingService

---
+++ Backend/src/main/java/com/eaglevision/Backend/service/PingService.java
@@ -0,0 +1,48 @@
+package com.eaglevision.Backend.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eaglevision.Backend.model.Buyer;
+import com.eaglevision.Backend.model.BuyerCheckPing;
+import com.eaglevision.Backend.model.Item;
+import com.eaglevision.Backend.model.Ping;
+import com.eaglevision.Backend.model.Vendor;
+import com.eaglevision.Backend.model.VendorResponsePing;
+import com.eaglevision.Backend.repository.PingRepository;
+import com.eaglevision.Backend.requests.CreatePingRequest;
+
+@Service
+public class PingService {
+	
+	private PingRepository pingRepository;
+	
+	@Autowired
+	public PingService(PingRepository pingRepository) {
+		this.pingRepository = pingRepository;
+	}
+	
+	public Ping createPing(Item item,CreatePingRequest createPingRequest) {
+		if(createPingRequest.getType()==0) {
+			Buyer buyer = createPingRequest.getBuyer();
+			BuyerCheckPing buyerCheckPing = new BuyerCheckPing(buyer,item);
+			return this.pingRepository.save(buyerCheckPing);
+		}
+		else
+		{
+			Vendor vendor = createPingRequest.getVendor();
+			VendorResponsePing vendorResponsePing = new VendorResponsePing(createPingRequest.getQuantity(),item,vendor);
+			return this.pingRepository.save(vendorResponsePing);
+		}
+	}
+	
+	public List<Ping> getAllPings(Integer itemId){
+		return this.pingRepository.findByItem_itemId(itemId);
+	}
+	
+	public Ping getPing(Integer itemId,Integer pingId){
+		return this.pingRepository.findByItem_itemIdAndPingId(itemId,pingId);
+	}
+}
