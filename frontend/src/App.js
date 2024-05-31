a9b08852f39380068e9002844c4fa4191fd171e3
giriharan13 <lionking0304channel@gmail.com>
2024-05-31 23:00:30 +0530

    Frontend : Update
    deleted and made some changes to files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import ListShops from './components/listshops/ListShops';
+import Shop from './components/shop/Shop';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import ListPings from './components/pings/ListPings';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header></Header>
+      <div className="container" style={{minHeight:"80vh"}}>
+        <Routes> 
+          <Route path="/shops" element={<ListShops/>}/>
+          <Route path="/shops/:id" element={<Shop/>}/>
+          <Route path="/shops/:shopId/items/:id/pings" element={<ListPings/>}/>
+        </Routes>
+      </div>
+      <Footer></Footer>
+    </BrowserRouter>
   );
 }
 
