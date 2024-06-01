408d9e5a6d952d1b7f2f4dfa0a5b6deeca2c3221
giriharan13 <lionking0304channel@gmail.com>
2024-06-01 23:04:50 +0530

    Frontend : Navbar
    added a nav bar in header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -2,9 +2,27 @@ import { Link } from "react-router-dom"
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 export default function Header(){
-    return <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-       <Link to="/" className="d-flex align-items-center m-2 mb-md-0 me-md-auto text-dark text-decoration-none"> 
-        <span className="fs-4">EagleVision</span>
-       </Link>
+    return <header className="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <Link to="/" className="navbar-brand"> 
+                <span className="fs-4">EagleVision</span>
+            </Link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="d-flex collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contact">Contact us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </header>
 }
