e6cfbd29a804adac0ca3de805c11edacfcd8c099
giriharan13 <lionking0304channel@gmail.com>
2024-06-19 13:34:27 +0530

    Frontend : Service
    modified service files

--- frontend/src/service/ApiClient.js
+++ frontend/src/service/ApiClient.js
@@ -5,4 +5,15 @@ const ApiClient = axios.create({
     "baseURL":"http://localhost:8080"
 })
 
+ApiClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('JWTToken'); 
+    if (token) {
+        config.headers['Authorization'] = 'Bearer ' + token;  
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+
 export default ApiClient;
--- frontend/src/service/BackendApi.js
+++ frontend/src/service/BackendApi.js
@@ -4,5 +4,11 @@ export const getShops = () => ApiClient.get("/shops");
 
 export const getShopById = (id) => ApiClient.get(`/shops/${id}`)
 
+export const getItemById = (shopId,id) => ApiClient.get(`/shops/${shopId}/items/${id}`)
+
 export const getPingsById = (shopId,id) => ApiClient.get(`/shops/${shopId}/items/${id}/pings`)
+
+export const authenticate = (username,password) => ApiClient.post(`/auth/login`,{},{headers:{
+    'Authorization': 'Basic '+ btoa(username + ':' + password)
+}})
 
