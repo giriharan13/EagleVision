dcb1df1cf3c6fd0a555828c395fafd749e8d2c42
giriharan13 <lionking0304channel@gmail.com>
2024-06-19 13:35:13 +0530

    Frontend : App.js
    modified App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
@@ -7,21 +7,53 @@ import Shop from './components/shop/Shop';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import ListPings from './components/pings/ListPings';
 import Home from './components/home/Home';
+import AuthProvider, { useAuth } from './security/AuthContext';
+import Login from './components/login/Login';
+
+function AuthenticatedRoute({children}){
+  const authContext = useAuth();
+  console.log(authContext.isAuthenticated);
+  if(authContext.isAuthenticated){
+    return children;
+  }
+  return <Navigate to="/login"/>
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header></Header>
-      <div className="container" style={{minHeight:"80vh"}}>
-        <Routes> 
-          <Route path="/" element={<Home/>}/>
-          <Route path="/shops" element={<ListShops/>}/>
-          <Route path="/shops/:id" element={<Shop/>}/>
-          <Route path="/shops/:shopId/items/:id/pings" element={<ListPings/>}/>
-        </Routes>
-      </div>
-      <Footer></Footer>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Header></Header>
+        <div className="container p-5" style={{height:"90vh"}}>
+          <Routes>
+            <Route path="/login" element={
+              <Login/>
+            }/>
+            <Route path="/home" element={
+              <AuthenticatedRoute>
+                <Home/>
+              </AuthenticatedRoute>
+            }/>
+            <Route path="/shops" element={
+              <AuthenticatedRoute>
+                <ListShops/>
+              </AuthenticatedRoute>
+            }/>
+            <Route path="/shops/:id" element={
+              <AuthenticatedRoute>
+                <Shop/>
+              </AuthenticatedRoute>
+            }/>
+            <Route path="/shops/:shopId/items/:id/pings" element={
+              <AuthenticatedRoute>
+                <ListPings/>
+              </AuthenticatedRoute>
+              }/>
+          </Routes>
+        </div>
+        <Footer></Footer>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
