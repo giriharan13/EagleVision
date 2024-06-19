60e7237d86e4376bab9db793dc0b0e70ca5f58ba
giriharan13 <lionking0304channel@gmail.com>
2024-06-19 12:37:19 +0530

    Backend : BackendApplication.java
    modified BackendApplication.java

--- Backend/src/main/java/com/eaglevision/Backend/BackendApplication.java
+++ Backend/src/main/java/com/eaglevision/Backend/BackendApplication.java
@@ -1,9 +1,9 @@
 package com.eaglevision.Backend;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,17 +13,17 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-	
+
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:3000");
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000").allowedMethods("*").allowedHeaders("*")
+						.allowCredentials(true);
 			}
 		};
 	}
 
 }
-
-
