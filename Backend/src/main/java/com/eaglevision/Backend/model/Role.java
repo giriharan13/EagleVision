46e6d112f597fd97ba00d0a15398dc473f29ab89
giriharan13 <lionking0304channel@gmail.com>
2024-06-19 12:25:19 +0530

    Backend : Model-Role.java
    adding a new model for representing the roles of the users

---
+++ Backend/src/main/java/com/eaglevision/Backend/model/Role.java
@@ -0,0 +1,46 @@
+package com.eaglevision.Backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "role", uniqueConstraints = @UniqueConstraint(columnNames = { "roleId", "name" }))
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer roleId;
+
+    @Column(unique = true)
+    private String name;
+
+    Role() {
+    }
+
+    Role(Integer roleId, String name) {
+        this.roleId = roleId;
+        this.name = name;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
