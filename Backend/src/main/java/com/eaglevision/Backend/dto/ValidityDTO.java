3e14fb476093ecbff8d9a4d6b2aa10b024e9269f
giriharan13 <lionking0304channel@gmail.com>
2024-06-29 12:26:50 +0530

    Backend - ValidityDTO.java
    added a new DTO

---
+++ Backend/src/main/java/com/eaglevision/Backend/dto/ValidityDTO.java
@@ -0,0 +1,15 @@
+package com.eaglevision.Backend.dto;
+
+public class ValidityDTO<T> {
+
+    private T field;
+
+    public T getField() {
+        return field;
+    }
+
+    public void setUsername(T field) {
+        this.field = field;
+    }
+
+}
