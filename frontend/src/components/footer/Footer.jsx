1f072d73f593418507519e189198adfeef20792e
giriharan13 <lionking0304channel@gmail.com>
2024-07-15 21:31:37 +0530

    Frontend - Footer
    modified footer component

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import './Footer.scss'
 
 export default function Footer(){
     return (
-        <footer className="footer bg-dark">
+        <footer className="footer bg-secondary">
             <p className="text-center text-white text-lg-start">Copyrights  CH13F @ 2024</p>
         </footer>
     )
