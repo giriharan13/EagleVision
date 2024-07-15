2af61e0988d3d61c6ec8eb1f2b7fe3c6ccc7d866
giriharan13 <lionking0304channel@gmail.com>
2024-07-15 21:32:41 +0530

    Frontend - Home
    modified Home component

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,9 +1,13 @@
 import { useNavigate } from "react-router-dom"
 import "./Home.css"
+import { useAuth } from "../../security/AuthContext";
+import { roles } from "../../config/Roles";
 
 export default function Home(){
 
     let navigate = useNavigate();
+
+    const authContext = useAuth();
 
     function navigateTo(path){
         navigate(path);
@@ -13,8 +17,17 @@ export default function Home(){
         <h1 className="welcome">Welcome to Eagle Vision🦅</h1>
         <h4 className="welcome">Menu</h4>
         <div className="menu">
+            { authContext.decoded?.scope && authContext.decoded.scope === roles.BUYER && <>
             <button className="btn btn-primary m-2" onClick={()=>{navigateTo("/shops")}}>Shops</button>
-            <button className="btn btn-primary m-2" onClick={()=>{navigateTo("/items")}}>Items</button> {/* Add a controller in backend,that uses a ML model to get the best selling items,and best shops*/}
+            <button className="btn btn-primary m-2" onClick={()=>{navigateTo("/items")}}>Items</button>
+            </> } {/* Add a controller in backend,that uses a ML model to get the best selling items,and best shops*/}
+            {
+                authContext.decoded?.scope && authContext.decoded.scope === roles.VENDOR &&
+                <>
+                <button className="btn btn-primary m-2" onClick={()=>{navigateTo("/myshops")}}>My Shops</button>
+                <button className="btn btn-primary m-2" onClick={()=>{navigateTo("/shops")}}>Pings</button>
+                </> 
+            }
         </div>
         
     </div>
