7f34a3af6f24437eec1153db4fe33e4ffca95bf1
giriharan13 <lionking0304channel@gmail.com>
2024-07-15 21:32:14 +0530

    Frontend - Header
    modified header component

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import { Link, useLocation, useNavigate } from "react-router-dom"
-import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+// import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import { useAuth } from "../../security/AuthContext"
+import toast from "react-hot-toast";
 
 export default function Header(){
 
@@ -14,13 +15,14 @@ export default function Header(){
 
     const logout = async ()=>{
         await authContext.logout();
+        toast.success("Logged out successfully!")
         navigate("/login")
     }
 
-    return <header className="navbar navbar-expand-lg navbar-dark bg-dark">
+    return <header className="navbar navbar-expand-lg navbar-dark bg-secondary">
         <div class="container-fluid">
             <Link to="/home" className="navbar-brand"> 
-                <span className="fs-4">EagleVision</span>
+                <p className="fs-4 text-white fw-bold">🦅EagleVision</p>
             </Link>
             {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -28,19 +30,19 @@ export default function Header(){
             <div class="d-flex collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <Link class="nav-link active" aria-current="page" to="/home">Home</Link>
+                        <Link class="nav-link active text-white" aria-current="page" to="/home">Home</Link>
                     </li>
                     <li class="nav-item">
-                        <Link class="nav-link" to="/about">About</Link>
+                        <Link class="nav-link text-white" to="/about">About</Link>
                     </li>
                     <li class="nav-item">
-                        <Link class="nav-link" to="/contact">Contact us</Link>
+                        <Link class="nav-link text-white" to="/contact">Contact us</Link>
                     </li>
                 </ul> 
             </div>
             <ul class="d-flex nav navbar-nav navbar-right">
                     {isAuthenticated && <li className="nav-item">
-                        <Link className="nav-link" onClick={logout}>Logout</Link>
+                        <Link className="nav-link text-primary" onClick={logout}>Logout</Link>
                         </li>}
                     {!isAuthenticated && location.pathname!=='/login' &&
                         <li class="nav-item">
