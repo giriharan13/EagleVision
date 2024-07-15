451403bdf4246368421ac71e423a33659adec553
giriharan13 <lionking0304channel@gmail.com>
2024-07-15 21:35:05 +0530

    Frontend - Pings
    modified a ping component

--- frontend/src/components/pings/ListPings.jsx
+++ frontend/src/components/pings/ListPings.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react"
-import { getItemById, getPingsById } from "../../service/BackendApi";
+import { createVendorPing, getItemById, getOwnerId, getPingsById } from "../../service/BackendApi";
 import { useParams } from "react-router-dom";
 import ShopImage from "./../../images/shop.jpg"
+import { useAuth } from "../../security/AuthContext";
+import { Field, Form, Formik } from "formik";
+import toast from "react-hot-toast";
 
 
 export default function ListPings(){
@@ -10,21 +13,38 @@ export default function ListPings(){
 
     const [item,setItem] = useState([]) ;
 
-    const {shopId,id} = useParams();
+    const {shopId,itemId} = useParams();
+
+    const [isOwner,setIsOwner] = useState(false)
+
+    const [refresh,setRefresh] = useState(false)
+
+    const authContext = useAuth();
+
+
+    const initialValues = {
+        quantity:"0",
+    }
 
 
     useEffect(()=>{
         
-        getPingsById(shopId,id).then((response)=>{
+        getPingsById(shopId,itemId).then((response)=>{
             refreshPings(response.data)
         }).catch((err)=>{
             console.log(err);
         })
 
-        getItemById(shopId,id).then((response)=>{
+        getItemById(shopId,itemId).then((response)=>{
             refreshItem(response.data);
         }).catch((err)=>{
             console.log(err);
+        })
+
+        getOwnerId(shopId).then((response)=>{
+            if(authContext.decoded.userId=== response.data) setIsOwner(true)
+        }).catch((error)=>{
+            console.log(error)
         })
 
 
@@ -35,34 +55,48 @@ export default function ListPings(){
         function refreshItem(item){
             setItem(item);
         }
-    },[id,shopId])
+    },[itemId,shopId,refresh])
+
+    async function handleReplyPing(pingId,values){
+        return createVendorPing(shopId,itemId,pingId,{
+            quantity:values.quantity,
+            userId:authContext.decoded.userId
+        }).then((response)=>{
+            setRefresh(!refresh)
+            toast.success("Response ping created successfully.");
+        }).catch((error)=>{
+            toast.error("An error has occurred");
+            console.log(error);
+        })
+    }
 
 
     return <div className="container">
     <div className="row">
         <div className="col-6">
         {(pings.length===0)?<h4>No pings on this item yet!</h4>:pings.map((ping)=>{
-            return (<div className={(ping.type===0)?"card border-warning mb-3":"card text-bg-success mb-3"} style={{maxWidth:"40rem"}} key={ping.pingId}>
-                <div className={"card-header"+((ping.type===0)?" text-warning":"")}>{(ping.type===0)?"Buyer ping":"Vendor Ping"}</div>
+            return (<div className="card border-warning mb-3" style={{maxWidth:"40rem"}} key={ping.pingId}>
+                <div className="card-header text-warning">Buyer ping</div>
                 <div className="card-body">
                     <h5 className="title"> By {ping.userName}</h5>
-                    <p className="card-text">{(ping.type===1) && "Quantity Available:"+ping.quantity}</p>
+
+                    {(ping.vendorResponsePing === null) ? <div>No response yet.</div> : 
+                    <div className="card border-success mb-3" style={{maxWidth:"40rem"}} >
+                        <div className="card-header">Vendor ping</div>
+                        <div className="card-body">
+                            <h5>Quantity : {ping.vendorResponsePing.quantity}</h5>
+                        </div>
+                    </div> }
+                    
                 </div>
+                {isOwner && ping.vendorResponsePing === null && <Formik initialValues={initialValues} enableReinitialize onSubmit={(values)=>handleReplyPing(ping.pingId,values)}>
+                                <Form>
+                                    <Field type="text" name="quantity"/>
+                                    <button className="button-primary" type="submit">reply</button>
+                                </Form>
+                            </Formik>}
             </div>);
         }).reverse()}
-        </div>
-        <div className="col-6 g-0">
-            <div className="container">
-                <div className="card border-primary mb3">
-                    <div className="card-header" >
-                        <img alt="test" src={ShopImage}  className="img-fluid rounded-start"></img> {/* adding a static image as of now*/}
-                    </div>
-                    <div className="card-body">
-                        <h5 className="card-title">{item.itemName}</h5>
-                        <p className="card-text">Price : {item.itemPrice}</p>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
     </div>
