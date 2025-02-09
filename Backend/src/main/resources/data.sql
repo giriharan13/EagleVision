276d91aeeed07c669cdbfea05fcac9b0b4034ce1
giriharan13 <giriharanraghu@gmail.com>
2025-02-09 20:23:08 +0530

    Backend - data.sql
    updating data.sql

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -1,5 +1,12 @@
+
 insert into role(role_id,name) values(1,'BUYER');
 
 insert into role(role_id,name) values(2,'VENDOR');
 
 insert into role(role_id,name) values(3,'ADMIN');
+
+insert into role(role_id,name) values(4,'ACE');
+
+insert into role(role_id,name) values(5,'GOD');
+
+insert into role(role_id,name) values(6,'VENDOR+');
